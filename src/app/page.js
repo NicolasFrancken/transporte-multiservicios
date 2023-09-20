d213019d62d7164df2db01c22f46c5bd113565d7
Nicolas Francken <franckennicolas@gmail.com>
2023-09-19 23:24:39 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -100,7 +100,7 @@ href="#envios"
           />
         </Button>
       </section>
-      <div className="hidden md:block py-24 lg:py-64 background2">
+      <div className="hidden lg:block py-24 lg:py-64 background2">
         <section className=" py-8 lg:py-12 bg-white/50">
           <ScrollCarousel />
         </section>
